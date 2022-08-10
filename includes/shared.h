dcd374233efeaac540331f3f245e747def9e46a8
Milvintsiss <milvintsiss@gmail.com>
2022-08-11 00:12:54 +0200

    Adds t_num and t_stacks structs

--- includes/shared.h
+++ includes/shared.h
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 04:59:40 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/08/07 05:00:10 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/08/11 00:12:09 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,18 @@
 # include "libft.h"
 # include <unistd.h>
 # include <stdlib.h>
+
+typedef struct s_num
+{
+	int			v;
+	char const	*str;
+	t_num		*below_num;
+}	t_num;
+
+typedef struct s_stacks
+{
+	t_num	*a;
+	t_num	*b;
+}	t_stacks;
 
 #endif
