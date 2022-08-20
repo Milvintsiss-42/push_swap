d84584d0c6c83c12ec13d29dd00c821580b8f929
Milvintsiss <milvintsiss@gmail.com>
2022-08-20 20:02:06 +0200

    Changes separate_stack methods to only cut in half

--- srcs_push_swap/sort_stack.c
+++ srcs_push_swap/sort_stack.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: milvintsiss <milvintsiss@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/12 07:01:23 by milvintsiss       #+#    #+#             */
-/*   Updated: 2022/08/17 04:05:49 by milvintsiss      ###   ########.fr       */
+/*   Created: 2022/08/20 20:00:24 by ple-stra          #+#    #+#             */
+/*   Updated: 2022/08/20 20:00:26 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,15 @@ t_num	*get_last_of_stack(t_num *stack)
 	return (stack);
 }
 
-void	separate_stack_a(t_stacks *stacks, double pos_r)
+void	separate_stack_a_med(t_stacks *stacks)
 {
 	int	pos;
 	int	mid_len;
 
-	if (!find_stack_relative_pos(&pos, stacks->a, stacks->len_a, pos_r))
+	if (!find_stack_median(&pos, stacks->a, stacks->len_a))
 		exception_exit(*stacks);
-	mid_len = stacks->len_a * (1 - pos_r);
-	if (stacks->len_a % 2 && pos_r <= 0.5)
+	mid_len = stacks->len_a * 0.5;
+	if (stacks->len_a % 2)
 		mid_len++;
 	while (stacks->len_a > mid_len)
 	{
@@ -50,15 +50,15 @@ void	separate_stack_a(t_stacks *stacks, double pos_r)
 	}
 }
 
-void	separate_stack_b(t_stacks *stacks, double pos_r)
+void	separate_stack_b_med(t_stacks *stacks)
 {
 	int	pos;
 	int	mid_len;
 
-	if (!find_stack_relative_pos(&pos, stacks->b, stacks->len_b, pos_r))
+	if (!find_stack_median(&pos, stacks->b, stacks->len_b))
 		exception_exit(*stacks);
-	mid_len = stacks->len_b * (1 - pos_r);
-	if (stacks->len_a % 2 && pos_r <= 0.5)
+	mid_len = stacks->len_b * 0.5;
+	if (stacks->len_a % 2)
 		mid_len++;
 	while (stacks->len_b > mid_len)
 	{
@@ -73,5 +73,5 @@ void	sort_stack_a(t_ps *ps)
 {
 	if (basic_cases(&ps->stacks))
 		return ;
-	separate_stack_a(&ps->stacks, 0.33);
+	separate_stack_a_med(&ps->stacks);
 }
