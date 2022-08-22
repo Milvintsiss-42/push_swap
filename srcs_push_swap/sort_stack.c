f6a9066066e4b25bb71b9494b061f23dcb13180d
Milvintsiss <milvintsiss@gmail.com>
2022-08-22 13:52:09 +0200

    Makes stacks of 5 numbers sorted in less than 12 moves

--- srcs_push_swap/sort_stack.c
+++ srcs_push_swap/sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 20:00:24 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/08/22 10:52:26 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/08/22 13:51:14 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,14 @@ static int	basic_cases(t_stacks *stacks)
 
 void	sort_stack_a(t_ps *ps)
 {
+	int	len_stack;
+
+	len_stack = ps->stacks.len_a;
 	if (basic_cases(&ps->stacks))
 		return ;
 	separate_stack_a_med(&ps->stacks);
-	push_all_but_med_and_max_in_b(ps);
+	if (len_stack > 7)
+		push_all_but_med_and_max_in_b(ps);
 	while (ps->stacks.len_b > 0)
 	{
 		apply_rots(find_best_move(ps->stacks), &ps->stacks);
