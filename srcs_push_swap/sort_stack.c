f85b14eb2156796f4919dbc2e75d4133f778b156
Milvintsiss <milvintsiss@gmail.com>
2022-09-04 20:26:57 +0200

    Optimizes sorting for 4 and 5 len stacks so they are sorted in 6 to 10 moves

--- srcs_push_swap/sort_stack.c
+++ srcs_push_swap/sort_stack.c
@@ -6,11 +6,40 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 20:00:24 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/08/23 10:33:12 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/04 20:24:55 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	four_or_five_stack(t_stacks *stacks)
+{
+	int	len_stack;
+
+	len_stack = stacks->len_a;
+	while (stacks->len_a > 3 - (len_stack == 4))
+	{
+		if (stacks->a->v <= 1)
+			push_b(stacks, 1);
+		else
+			rotate_a(stacks, 1, 0);
+	}
+	if (stacks->b->v == 0)
+		swap_b(stacks, 1);
+	if (len_stack == 5)
+	{
+		if (stacks->a->v == 4)
+			rotate_a(stacks, 1, 0);
+		if (stacks->a->below_num->v == 4)
+			rotate_a(stacks, 1, 1);
+		if (stacks->a->v == 3)
+			swap_a(stacks, 1);
+	}
+	else if (stacks->a->v == 3)
+		swap_a(stacks, 1);
+	push_a(stacks, 1);
+	push_a(stacks, 1);
+}
 
 static int	basic_cases(t_stacks *stacks)
 {
@@ -29,6 +58,11 @@ static int	basic_cases(t_stacks *stacks)
 			rotate_a(stacks, 1, 1);
 		if (stacks->a->v == 1)
 			swap_a(stacks, 1);
+		return (1);
+	}
+	if (stacks->len_a <= 5)
+	{
+		four_or_five_stack(stacks);
 		return (1);
 	}
 	return (0);
