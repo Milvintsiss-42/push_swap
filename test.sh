dbcdd6121672d597eec2c3f668dd1953042d2368
Milvintsiss <milvintsiss@gmail.com>
2022-09-25 17:20:11 +0200

    Updates test script

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #/bin/bash
 ARG=$(seq $1 | sort -R | tr '\n' ' ')
+echo "Testing with the following sequence: $ARG"
+
+echo -n "Status: "
 valgrind --quiet ./push_swap $ARG | valgrind --quiet ./checker $ARG
+
+echo -n "Operations count: "
+./push_swap $ARG | wc -l
